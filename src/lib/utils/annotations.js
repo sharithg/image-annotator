8870ca5365b7474c004f8456ac4ecd3db34a1163
Sharith Godamanna <sharithg@bu.edu>
2022-11-12 17:24:21 -0500

    Remove lingering js files

--- src/lib/utils/annotations.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.validateAnnotations = void 0;
-var validateAnnotations = function (annotations) {
-    return annotations;
-};
-exports.validateAnnotations = validateAnnotations;
